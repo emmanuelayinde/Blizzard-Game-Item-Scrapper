2a39482f31cecc86c29446f7efd2355e524f50ca
engr-solution <dah.solu.tion7@gmail.com>
2022-09-01 01:33:18 +0100

    add chromedriver autoinstaller

--- main.py
+++ main.py
@@ -5,22 +5,23 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from keep_alive import alive
 from utils.scrapper import battle_shop_scrapper
+import chromedriver_autoinstaller
 
-# CONFIG FOR DEVELOPMENT ENV
-PATH = "C:\Program Files (x86)\chromedriver.exe"    # Replace with your chromedriver path 
+chromedriver_autoinstaller.install()
 
 # CONFIG FOR PRODUCTION ENV
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--disable-dev-shm-usage")
-# chrome_options.add_argument("--no-sandbox")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 
+alive()
 
 while True:
-    driver = webdriver.Chrome(PATH)               # CONFIG FOR DEVELOPMENT ENV 
-    # driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)            
 
     print('Scraping shop.battle.net...........................')
     battle_shop_scrapper(driver, WebDriverWait, By, EC)
