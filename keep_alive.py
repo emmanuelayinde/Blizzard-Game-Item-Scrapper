7775265b802f41b161b9bd53d378fa0b13259066
engr-solution <dah.solu.tion7@gmail.com>
2022-09-03 21:51:27 +0100

    add worker to Procfile

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,18 @@
-# from flask import Flask
-# from threading import Thread
+import os
+from flask import Flask
+from threading import Thread
 
-# app = Flask('')
+app = Flask('')
 
-# @app.route('/')
-# def main():
-#   return "Your bot is alive!"
+@app.route('/')
+def main():
+  return "Your bot is alive!"
 
-# def run():
-#     app.run(host="0.0.0.0", port=8080)
+def run():
+    # app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 17995))
+    app.run(host='0.0.0.0', port=port)
 
-# def alive():
-#     server = Thread(target=run)
-#     server.start()
+def alive():
+    server = Thread(target=run)
+    server.start()
